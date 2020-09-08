ab6739278bb8ac9dddd8a0dd5e6be47231b48247
Beni Madhab Kayal <benimadhabkayal@Benis-MacBook-Pro.local>
2020-09-08 15:23:53 +0530

    home

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route} from 'react-router-dom';
 import Login from './components/Login';
 import Admin from './components/Admin';
 import Logout from './components/Logout';
+import HomePage from './components/HomePage';
 
 class App extends Component {
   render() {
@@ -10,7 +11,8 @@ class App extends Component {
     return (
       <BrowserRouter>
       <Switch>
-        <Route exact path="/" component={Login}></Route>
+        <Route exact path="/" component={HomePage}></Route>
+        <Route exact path="/Login" component={Login}></Route>
         <Route exact path="/Admin" component={Admin}></Route>
         <Route exact path="/Logout" component={Logout}></Route>
       </Switch>
---
+++ src/components/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+class HomePage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( <div>
+            <H1>HomePage</H1>
+        </div>);
+    }
+}
+ 
+export default HomePage;
