8e4cd3de33426311e95fc19d7f3f84923b4e367c
Beni Madhab Kayal <benimadhab11@gmail.com>
2020-09-13 03:19:15 +0530

    one comp clean

--- src/components/PageContainer.js
+++ src/components/PageContainer.js
@@ -8,78 +8,46 @@ class PageContainer extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			displayedNote: "new",
-			posts: [],
-			editorState: EditorState.createEmpty()
+			posts: []
 		};
 	}
 
 	componentDidMount() {
-		if (this.props.note === null) {
-			this.setState({
-				displayedNote: "new",
-				posts: [],
-				editorState: EditorState.createEmpty()
-			})
-		} else {
-			console.log(this.props)
-				this.setState({
-					displayedNote: this.props.note.id,
-					posts: this.props.posts,
-					editorState: EditorState.createWithContent(convertFromRaw(JSON.parse(this.props.note.content)))
-			})
-			console.log(this.props)
-		}
-}
+		this.setState({
+				posts: this.props.posts
+		})
+	}
 
-componentDidUpdate(prevProps, prevState) {
-
-
-	if (prevProps.note == null && !!this.props.note) {
-		//this.props.loadNote
+	componentDidUpdate(prevProps, prevState) {
 		this.props.posts.map(
 			post => 
 			post.content = EditorState.createWithContent(convertFromRaw(JSON.parse(post.content)))
 			)
-		console.log(this.props.note.content)
 		this.setState({
-			displayedNote: this.props.note.id,
-			editorState: this.props.note.content,
 			posts: this.props.posts
 		})
-		console.log(this.state.editorState)
 	}
-}
+
 
 	
 
 
 	render() {
 		return (
-			<div className="editorContainer">
-				
+			<div className="displayPostContainer">
 				<div>
-				{this.state.posts.map(post =>
-				<Editor id={post.id}
-				editorState={post.content}
-				readOnly
-				/>
-                
-                )}
+				{this.state.posts.map(post => <Editor id={post.id} editorState={post.content} readOnly/>)}
 				</div>
 			</div>
 		);
 	}
+
 }
 
+
 function mapStateToProps(state, props) {
-	console.log(this.props)
 	return {
-		note: state.displayedNote,
-		posts: state.posts,
-		fetching : state.fetching,
-		fetched: state.fetched,
-		error: state.error,
+		posts: state.posts
 	};
 }
 
