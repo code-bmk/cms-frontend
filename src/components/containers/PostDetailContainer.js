b638fa9e99a67e1dc083819c6abe634d58a606ea
Beni Madhab Kayal <benimadhab11@gmail.com>
2020-09-14 00:33:52 +0530

    fix

--- src/components/containers/PostDetailContainer.js
+++ src/components/containers/PostDetailContainer.js
@@ -2,51 +2,54 @@ import React from "react";
 import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
 import * as Actions from "../../actions";
-import {
-  Editor,
-  EditorState,
-  convertFromRaw,
-} from "draft-js";
+import { Editor, EditorState, convertFromRaw } from "draft-js";
 
 class PostDetailContainer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-        displayedNote: {content:''}
+      displayedNote: { content: "" },
     };
   }
 
   componentDidMount() {
     if (this.props.displayedNote === null) {
       this.setState({
-        displayedNote: {content:''}
+        displayedNote: { content: "" },
       });
     } else {
-      console.log(this.props);
-
-      this.props.displayedNote.content = EditorState.createWithContent(convertFromRaw(JSON.parse(this.props.displayedNote.content)))
       this.setState({
-        displayedNote: this.props.displayedNote
+        displayedNote: this.props.displayedNote,
+      });
+    }
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.displayedNote == null && !!this.props.displayedNote) {
+      this.props.displayedNote.content = EditorState.createWithContent(
+        convertFromRaw(JSON.parse(this.props.displayedNote.content))
+      );
+
+      this.setState({
+        displayedNote: this.props.displayedNote,
       });
     }
   }
 
   render() {
     return (
-    <div className="container is-fluid">
+      <div className="container is-fluid">
         <h1 class="title">{this.state.displayedNote.title}</h1>
-         <Editor editorState={this.state.displayedNote.content}></Editor>
+        <Editor id={this.state.displayedNote.id} editorState={this.state.displayedNote.content} readOnly/>
       </div>
     );
   }
 }
 
 function mapStateToProps(state, props) {
-  console.log(this.props);
   return {
     displayedNote: state.displayedNote,
     fetching: state.fetching,
-    fetched: state.fetched
+    fetched: state.fetched,
   };
 }
 
@@ -54,4 +57,7 @@ function mapDispatchToProps(dispatch) {
   return bindActionCreators(Actions, dispatch);
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(PostDetailContainer);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(PostDetailContainer);
