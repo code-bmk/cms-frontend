290d44cc42151a852907768f22418029856df6e3
Beni Madhab Kayal <benimadhab11@gmail.com>
2020-09-13 17:30:10 +0530

    one comp clean

--- src/components/Edit.js
+++ src/components/Edit.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import EditableContainer from "./EditableContainer";
+import EditableContainer from "./containers/EditableContainer";
 import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
 import * as Actions from "../actions";
