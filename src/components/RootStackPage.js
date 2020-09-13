dea7075dc2d0e99a2871d64833443624ed342892
Beni Madhab Kayal <benimadhab11@gmail.com>
2020-09-13 19:26:55 +0530

    one comp clean

--- src/components/RootStackPage.js
+++ src/components/RootStackPage.js
@@ -1,15 +1,9 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import Display from './Display';
 
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-    </View>
-  );
-}
+
 
 const Stack = createStackNavigator();
 
@@ -17,7 +11,7 @@ function RootStackPage() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Display" component={Display} />
       </Stack.Navigator>
     </NavigationContainer>
   );
