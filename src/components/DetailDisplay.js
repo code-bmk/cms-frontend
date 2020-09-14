e2a8d40532227c0cc2ff5101834c5b95c34b74c3
Beni Madhab Kayal <benimadhab11@gmail.com>
2020-09-15 03:31:47 +0530

    fix

--- src/components/DetailDisplay.js
+++ src/components/DetailDisplay.js
@@ -8,7 +8,7 @@ class DetailDisplay extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      displayedNote: {content:EditorState.createEmpty()},
+      displayedNote: { content: EditorState.createEmpty() },
       blogPost: "",
     };
   }
@@ -20,7 +20,7 @@ class DetailDisplay extends Component {
 
     if (this.props.displayedNote === null) {
       this.setState({
-        displayedNote: {content:EditorState.createEmpty()},
+        displayedNote: { content: EditorState.createEmpty() },
         blogPost: "",
       });
     } else {
@@ -32,22 +32,29 @@ class DetailDisplay extends Component {
     }
   }
   componentDidUpdate(prevProps, prevState) {
-    // const {
-    //   match: { params },
-    // } = this.props;
-    // this.props.loadOneNote(params.id);
-    // let displayedNote = { ...this.state.displayedNote };
-    // displayedNote.content = EditorState.createWithContent(
-    //   convertFromRaw(JSON.parse(this.props.displayedNote.content))
-    // );
-    // this.setState({ displayedNote });
+    const {
+      match: { params },
+    } = this.props;
+
+    if (prevProps.displayedNote == null && !!this.props.displayedNote) {
+      this.props.loadOneNote(params.id);
+      let displayedNote = { ...this.state.displayedNote };
+      displayedNote.content = EditorState.createWithContent(
+        convertFromRaw(JSON.parse(this.props.displayedNote.content))
+      );
+      this.setState({ displayedNote });
+    }
   }
 
   render() {
     return (
       <div class="container is-fluid">
         <h1 class="title">{this.state.displayedNote.title}</h1>
-        <Editor id={this.state.displayedNote.id} editorState={this.state.displayedNote.content} readOnly/>
+        <Editor
+          id={this.state.displayedNote.id}
+          editorState={this.state.displayedNote.content}
+          readOnly
+        />
       </div>
     );
   }
