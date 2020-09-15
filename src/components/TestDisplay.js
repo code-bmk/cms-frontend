5c734520d8da2efdaaf9c1f11670aa5c50a76903
Beni Madhab Kayal <benimadhab11@gmail.com>
2020-09-15 19:18:44 +0530

    clean

--- src/components/TestDisplay.js
+++ src/components/TestDisplay.js
@@ -1,17 +1,17 @@
-import React, { Component } from 'react';
-import { withLastLocation } from 'react-router-last-location';
-import { getPostDetailsById } from '../services/herokuApi';
+import React, { Component } from "react";
+import { withLastLocation } from "react-router-last-location";
+import { getPostDetailsById } from "../services/herokuApi";
 import { Editor, EditorState, convertFromRaw } from "draft-js";
 
 const blogStyle = {
-    width: "60%"
-  };
+  width: "60%",
+};
 
 class TestDisplay extends Component {
   state = {
-    postInfo: {content: EditorState.createEmpty()},
+    postInfo: { content: EditorState.createEmpty() },
     loading: true,
-    error: true
+    error: true,
   };
 
   async componentDidMount() {
@@ -19,12 +19,12 @@ class TestDisplay extends Component {
       try {
         const postInfo = await getPostDetailsById(this.props.match.params.id);
         postInfo.content = EditorState.createWithContent(
-            convertFromRaw(JSON.parse(postInfo.content))
-          );
+          convertFromRaw(JSON.parse(postInfo.content))
+        );
         this.setState({
           loading: false,
           postInfo,
-          error: false
+          error: false,
         });
       } catch (err) {
         this.setState({ loading: false, error: true });
@@ -34,40 +34,39 @@ class TestDisplay extends Component {
 
   render() {
     const { postInfo, loading, error } = this.state;
-   
 
     let movieDetails = null;
 
     if (error) {
-      movieDetails = (
-
-        <h1 class="title is-1">Error</h1>
-   
-      );
+      movieDetails = <h1 class="title is-1">Error</h1>;
     }
 
     if (loading) {
-      movieDetails = (
-        <h1 class="title is-1">Loading</h1>
-      );
+      movieDetails = <h1 class="title is-1">Loading</h1>;
     }
 
     if (!loading && postInfo) {
       movieDetails = (
-       
         <div>
-        <h1 class="title is-1">{postInfo.title}</h1>
-        <Editor
-          id={postInfo.id}
-          editorState={postInfo.content}
-          readOnly
-        />
+          <section class="hero is-primary is-dark is-bold">
+            <div class="hero-body">
+              <div class="container">
+                <p class="title">{postInfo.title}</p>
+                <p class="subtitle">{postInfo.date}</p>
+              </div>
+            </div>
+          </section>
+
+          <Editor id={postInfo.id} editorState={postInfo.content} readOnly />
         </div>
-   
       );
     }
 
-    return (<div class="container is-fluid" style={blogStyle}>{movieDetails}</div>);
+    return (
+      <div class="container is-fluid" style={blogStyle}>
+        {movieDetails}
+      </div>
+    );
   }
 }
 
